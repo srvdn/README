271c4610d2ea8ecc3b6fa8e590243b2c5cc0e3c2
srvdn <suierovdaden@gmail.com>
2026-09-20 14:40:47 +0000

    p

--- Hello!.pl
+++
@@ -1,7 +0,0 @@
-Hello!
-
-My name is Suierov Daden and this is my personal Git. 
-I am currently transitioning from my previous career 
-(I used to be a Chief Officer on a merchant vessel)
-and here I will test what I've been learning on my spare time
-
